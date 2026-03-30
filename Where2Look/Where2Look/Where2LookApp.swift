ff1f7566733b1a8ec66cc1ee1df48c4a266e8d46
Bill Weatherwax <52289148+waxcruzcoder@users.noreply.github.com>
2026-03-30 07:14:26 -0700

    Initial SwiftUI app scaffold

---
+++ Where2Look/Where2Look/Where2LookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Where2LookApp.swift
+//  Where2Look
+//
+//  Created by Bill Weatherwax on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Where2LookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Where2Look/Where2LookTests/Where2LookTests.swift
@@ -0,0 +1,19 @@
+//
+//  Where2LookTests.swift
+//  Where2LookTests
+//
+//  Created by Bill Weatherwax on 3/30/26.
+//
+
+import Testing
+@testable import Where2Look
+
+struct Where2LookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
